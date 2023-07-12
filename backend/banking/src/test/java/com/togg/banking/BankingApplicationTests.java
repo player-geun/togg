29f872a694fe30b092512f09a5c46799bb96b8a0
player-geun <player_geun@naver.com>
2023-07-12 14:45:00 +0900

    feat : 프로젝트 초기화

---
+++ backend/banking/src/test/java/com/togg/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togg.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
