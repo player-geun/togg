75da7f71ed59e45392bcfe2fa714565c5e1223cd
player-geun <player_geun@naver.com>
2023-07-14 10:03:25 +0900

    feat : JWT를 기반으로 인증 기능 필터를 구현

---
+++ backend/banking/src/main/java/com/togg/banking/auth/application/JwtAuthenticationFilter.java
@@ -0,0 +1,102 @@
+package com.togg.banking.auth.application;
+
+import com.togg.banking.member.domain.Member;
+import com.togg.banking.member.domain.MemberRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String NO_CHECK_URL = "/login";
+    private static final String BEARER = "Bearer ";
+
+    private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
+
+    @Value("${jwt.access.header}")
+    private String accessHeader;
+
+    @Value("${jwt.refresh.header}")
+    private String refreshHeader;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String refreshToken = extractToken(request, refreshHeader)
+                .filter(jwtProvider::isValidToken)
+                .orElse(null);
+        if (refreshToken == null) {
+            authenticateWithAccessToken(request);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        authenticateWithRefreshToken(response, refreshToken);
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> extractToken (HttpServletRequest request, String header) {
+        return Optional.ofNullable(request.getHeader(header))
+                .filter(token -> token.startsWith(BEARER))
+                .map(token -> token.replace(BEARER, ""));
+    }
+
+    private void authenticateWithAccessToken(HttpServletRequest request) {
+        String email = extractToken(request, accessHeader)
+                .map(jwtProvider::extractEmail)
+                .orElseThrow();
+        Member member = memberRepository.getByEmail(email);
+        saveAuthentication(member);
+    }
+
+    private void saveAuthentication(Member member) {
+        UserDetails userDetails = User.builder()
+                .username(member.getEmail())
+                .build();
+
+        GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null, authoritiesMapper.mapAuthorities(userDetails.getAuthorities()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void authenticateWithRefreshToken(HttpServletResponse response, String refreshToken) {
+        Member member = memberRepository.getByRefreshToken(refreshToken);
+        String reissuedRefreshToken = reissueRefreshToken(member);
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        String accessToken = jwtProvider.createAccessToken(member.getEmail());
+        response.setHeader(accessHeader, accessToken);
+        response.setHeader(refreshHeader, reissuedRefreshToken);
+    }
+
+    private String reissueRefreshToken(Member member) {
+        String refreshToken = jwtProvider.createRefreshToken();
+        member.changeRefreshToken(refreshToken);
+        memberRepository.save(member);
+        return refreshToken;
+    }
+}
---
+++ backend/banking/src/main/java/com/togg/banking/member/domain/MemberRepository.java
@@ -0,0 +1,25 @@
+package com.togg.banking.member.domain;
+
+import com.togg.banking.common.exception.NoSuchEntityException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+    default Member getByEmail(String email) {
+        return findByEmail(email)
+                .orElseThrow(() -> new NoSuchEntityException("해당 이메일의 회원이 존재하지 않습니다."));
+    }
+
+    Optional<Member> findByRefreshToken(String refreshToken);
+
+    default Member getByRefreshToken(String refreshToken) {
+        return findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new NoSuchEntityException("해당 리프레시 토큰의 회원이 존재하지 않습니다."));
+    }
+}
