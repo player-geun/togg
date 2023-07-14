d154220ea2af1e0f2e16bab9d1c46e52bd46c62e
player-geun <player_geun@naver.com>
2023-07-14 23:15:33 +0900

    feat : Social 로그인에 따른 사용자 정보를 받는 dto 구현

---
+++ backend/banking/src/main/java/com/togg/banking/auth/dto/GoogleUserInfo.java
@@ -0,0 +1,20 @@
+package com.togg.banking.auth.dto;
+
+import java.util.Map;
+
+public class GoogleUserInfo extends UserInfo {
+
+    public GoogleUserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+}
