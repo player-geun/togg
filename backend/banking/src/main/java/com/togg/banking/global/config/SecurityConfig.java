af3d52a69ad2de66e7fb9eacb0d59182e9374733
player-geun <player_geun@naver.com>
2023-07-15 13:10:44 +0900

    feat : security config 설정 클래스 구현

---
+++ backend/banking/src/main/java/com/togg/banking/global/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.togg.banking.global.config;
+
+import com.togg.banking.auth.application.CustomOAuth2UserService;
+import com.togg.banking.auth.application.JwtProvider;
+import com.togg.banking.auth.presentation.JwtAuthenticationFilter;
+import com.togg.banking.auth.presentation.OAuth2LoginSuccessHandler;
+import com.togg.banking.member.application.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtProvider jwtProvider;
+    private final MemberService memberService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .authorizeHttpRequests(request -> {request
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated();
+                });
+
+        http
+                .oauth2Login(oauth2 -> {oauth2
+                           .successHandler(oAuth2LoginSuccessHandler)
+                           .userInfoEndpoint(endpoint -> endpoint.userService(customOAuth2UserService));
+                });
+
+        http
+                .addFilterAfter(jwtAuthenticationProcessingFilter(), LogoutFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationProcessingFilter() {
+        return new JwtAuthenticationFilter(jwtProvider, memberService);
+    }
+}
