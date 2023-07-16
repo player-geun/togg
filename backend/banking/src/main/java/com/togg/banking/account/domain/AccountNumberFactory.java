02808fe62317ad80ab9fa51b05c35f68f6cbdb9b
player-geun <player_geun@naver.com>
2023-07-16 17:49:48 +0900

    refactor : 계좌번호 생성 클래스의 네이밍 변경

--- backend/banking/src/main/java/com/togg/banking/account/domain/AccountNumberFactory.java
+++ backend/banking/src/main/java/com/togg/banking/account/domain/AccountNumberFactory.java
@@ -2,7 +2,7 @@ package com.togg.banking.account.domain;
 
 import java.util.Random;
 
-public class AccountFactory {
+public class AccountNumberFactory {
 
     private final static String BANKBOOK_NUMBER = "1000";
     private final static Random RANDOM = new Random();
