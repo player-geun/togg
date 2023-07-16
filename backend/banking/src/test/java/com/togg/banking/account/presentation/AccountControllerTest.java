ce5798499b9dc83414924f2f97192ac18ac2cf7f
player-geun <player_geun@naver.com>
2023-07-16 10:53:30 +0900

    refactor : 컨트롤러 테스트에 필요한 공통 부분을 별도의 추상 클래스로 분리하여 상속받는 것으로 변경

--- backend/banking/src/test/java/com/togg/banking/account/presentation/AccountControllerTest.java
+++ backend/banking/src/test/java/com/togg/banking/account/presentation/AccountControllerTest.java
@@ -1,33 +1,17 @@
 package com.togg.banking.account.presentation;
 
-import com.togg.banking.account.application.AccountService;
 import com.togg.banking.account.dto.AccountResponse;
+import com.togg.banking.common.ControllerTest;
 import com.togg.banking.common.WithCustomMockUser;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc(addFilters = false)
-@WebMvcTest(AccountController.class)
-class AccountControllerTest {
-
-    private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
-    private static final String AUTHORIZATION_HEADER_VALUE = "Bearer aaaaaaaa.bbbbbbbb.cccccccc";
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private AccountService accountService;
+class AccountControllerTest extends ControllerTest {
 
     @WithCustomMockUser
     @Test
--- backend/banking/src/test/java/com/togg/banking/auth/presentation/AuthControllerTest.java
+++ backend/banking/src/test/java/com/togg/banking/auth/presentation/AuthControllerTest.java
@@ -1,44 +1,19 @@
 package com.togg.banking.auth.presentation;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.togg.banking.auth.application.JwtProvider;
 import com.togg.banking.auth.dto.SignUpRequest;
 import com.togg.banking.auth.dto.SignUpResponse;
+import com.togg.banking.common.ControllerTest;
 import com.togg.banking.common.WithCustomMockUser;
-import com.togg.banking.member.application.MemberService;
 import com.togg.banking.member.domain.InvestmentType;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@AutoConfigureMockMvc(addFilters = false)
-@WebMvcTest(AuthController.class)
-class AuthControllerTest {
-
-    private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
-    private static final String AUTHORIZATION_HEADER_VALUE = "Bearer aaaaaaaa.bbbbbbbb.cccccccc";
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
-    private MemberService memberService;
-
-    @MockBean
-    private JwtProvider jwtProvider;
+class AuthControllerTest extends ControllerTest {
 
     @WithCustomMockUser
     @Test
